1cf9d9360688913b1276a6fdaed6f7e9d3d18bb8
MAYAGALAXY-B\MayaGalaxy <c01153492f@edu.teu.ac.jp>
2017-12-20 22:50:43 +0900

    #add ファイルパス管理機能を追加しました

---
+++ VocaloidTimeSignal/NameManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocaloidTimeSignal
+{
+    enum language
+    {
+        /// <summary>
+        /// 心華（台湾中国語）
+        /// </summary>
+        XinHua_tw,
+        /// <summary>
+        /// 初音ミク（日本語）
+        /// </summary>
+        Miku_jp
+    }
+
+    class NameManager
+    {
+        private language lng;
+        private string voiceDir;
+        private string lngDir;
+
+        public NameManager(language lng,string voiceDir)
+        {
+            this.lng = lng;
+            this.voiceDir = voiceDir;
+
+            switch(lng)
+            {
+                case language.XinHua_tw: lngDir = voiceDir + "\\XinHua"; break;
+                case language.Miku_jp: lngDir = voiceDir + "\\Miku"; break;
+            }
+        }
+
+
+        public string[] GetHourMin(DateTime dt)
+        {
+            return GetHourMin(dt.Hour, dt.Minute);
+        }
+
+        public string[] GetHourMin(int h,int m)
+        {
+            var dirs = new string[1];
+
+            if(m == 0)
+            {
+                dirs[0] = lngDir + "\\hn" + h + ".wav";
+                return dirs;
+            }
+            else if (m % 10 == 0)
+            {
+                dirs = new string[2];
+                dirs[0] = lngDir + "\\hc" + h + ".wav";
+                dirs[1] = lngDir + "\\mt" + (m / 10) + ".wav";
+                return dirs;
+            }
+            else if (m < 10)
+            {
+                dirs = new string[2];
+                dirs[0] = lngDir + "\\hc" + h + ".wav";
+                dirs[1] = lngDir + "\\mn" + m + ".wav";
+                return dirs;
+            }
+
+            dirs = new string[3];
+            dirs[0] = lngDir + "\\hc" + h + ".wav";
+            dirs[1] = lngDir + "\\mc" + (m / 10) + ".wav";
+            dirs[2] = lngDir + "\\mn" + m + ".wav";
+            return dirs;
+        }
+
+        public string[] GetAlarm()
+        {
+            var files = new string[1];
+            files[0] = lngDir + "\\alarm.wav";
+            return files;
+        }
+
+    }
+}
---
+++ VocaloidTimeSignal/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VocaloidTimeSignal
+{
+    static class Player
+    {
+
+        public static bool Play()
+        { return Play(DateTime.Now); }
+
+        public static bool Play(DateTime time)
+        {
+            //数字（時）
+
+            //時
+
+
+            //数字(分)
+
+            //分
+
+
+            return false;
+        }
+
+        private static bool PlaySingleSound()
+        {
+            try
+            {
+                var ply = new System.Media.SoundPlayer("hogehoge.wav");
+                ply.PlaySync();
+            }
+            catch(Exception e)
+            {　MessageBox.Show(e.Message,"音の再生に失敗しました");　return false; }
+            return true;
+        }
+
+    }
+}
